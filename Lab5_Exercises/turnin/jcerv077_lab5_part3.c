bcfd69acfd06e31f69424756fb1ab759e46a9219
JoseC678 <cervantesjose657@gmail.com>
2021-10-17 13:38:50 -0700

    Added Video Link

--- Lab5_Exercises/turnin/jcerv077_lab5_part3.c
+++ Lab5_Exercises/turnin/jcerv077_lab5_part3.c
@@ -1,11 +1,12 @@
-/*	Author: lab
+/*	Author: jcerv077
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab #5  Exercise #3
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+ * Demo Link: https://www.youtube.com/watch?v=H_tDDIZYmgE
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
