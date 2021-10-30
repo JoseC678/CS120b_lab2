e456b5c7e551509c722fdf82f09622fd58603ec5
JoseC678 <cervantesjose657@gmail.com>
2021-10-29 18:42:01 -0700

    PART1

---
+++ Lab8_Exercises/source/main.c
@@ -0,0 +1,38 @@
+/*	Author: lab
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+
+#endif
+
+void ADC_init(){
+    ADCSRA |= (1 << ADEN | (1 << ADSC) | (1 << ADATE));
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0x00;
+    DDRD = 0xFF; PORTD = 0x00;
+
+    ADC_init();
+
+
+    unsigned short x = 0x00;
+    /* Insert your solution below */
+    while (1) {
+    x = ADC;
+    PORTB = (char)(x & 0xFF); //PORTB = 0xFF
+    PORTD = (char)((x >> 8) & 0x03);//PORTD == 0x03 shift over 8 right ;
+
+    }
+    return 1;
+}
