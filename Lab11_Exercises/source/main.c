f2b09aebc2897a331ec2df3a1da1a1e83d5be35e
JoseC678 <cervantesjose657@gmail.com>
2021-11-13 15:37:57 -0800

    Part2

--- Lab11_Exercises/source/main.c
+++ Lab11_Exercises/source/main.c
@@ -1,7 +1,7 @@
 /*	Author: lab Jose Cervantes
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #11  Exercise #1
+ *	Assignment: Lab #11  Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
