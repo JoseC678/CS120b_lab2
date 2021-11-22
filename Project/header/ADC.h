2e3c50dace32b9be393a55deb1c5c7196b468bde
JoseC678 <cervantesjose657@gmail.com>
2021-11-21 19:39:02 -0800

    ADC.h

--- Project/header/ADC.h
+++ Project/header/ADC.h
@@ -2,11 +2,11 @@
 #define ADC_H
 
 void InitADC(){
-	ADMUX=(1<<REFS0);//Set REFS0 on for the ADMUX to select the AVCC with external 
+    ADMUX=(1<<REFS0);//Set REFS0 on for the ADMUX to select the AVCC with external 
                      //capaitor at AREF pin
 
     //
-	ADCSRA=(1<<ADEN)|(1<<ADPS0)|(1<<ADPS1)|(1<<ADPS2); 	
+    ADCSRA=(1<<ADEN)|(1<<ADPS0)|(1<<ADPS1)|(1<<ADPS2); 	
     //ADEN = on than it enables ADC  to begin the conversion
     //Set ADPS0/1/2 to 1 so that we get the full 10 bit rsolution
     //of CK/128 inbetween 50kHz and 200kHz to get maximum resolution
@@ -18,7 +18,7 @@ unsigned short ADCvalue(unsigned char channel){
     ADMUX = (ADMUX & 0xFF)|channel; //clear ADMUX to set the corect ADC 
     ADCSRA|=(1<<ADSC); //Enable ADC 
     while((ADCSRA)&(1<<ADSC));  //Wait unit ADSC is set in ADCSRA
-    return(ADC);        
+    return(ADC);   //Return        
 }
 
 #endif //ADC_H
