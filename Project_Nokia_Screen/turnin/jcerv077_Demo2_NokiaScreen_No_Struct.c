1436bca2201f4f6883592bd4f589bb6a9446f5f2
JoseC678 <cervantesjose657@gmail.com>
2021-11-28 20:08:15 -0800

    Nokia screen without Joystick and Struct

---
+++ Project_Nokia_Screen/turnin/jcerv077_Demo2_NokiaScreen_No_Struct.c
@@ -0,0 +1,165 @@
+/*	Author: Jose Cervantes
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ *
+ *  Demo Link: https://drive.google.com/drive/folders/1452OogB5nVNzuja2EHKyvhxzJiOLQd_Y?usp=sharing
+ *
+ *
+ * Or if top dosent work on youtube
+ *
+ *  Demo Link: https://www.youtube.com/channel/UCQNx-GgmXaHu-ebYzAjvanA
+ *
+ */
+#include <avr/io.h>
+#include <util/delay.h>
+#include <string.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#include "../header/font.h"
+#include "../header/timer.h"
+#endif
+
+
+///Define value bits To activate SPI and MOSI
+#define SS PB4
+#define RST PB0
+#define DC PB1
+
+void SPI_Init();
+void SPI_Transmit(char cData);
+void init_Screen();
+void clear_Screen();
+void Snd_Nokia_Instructions(char command);
+void Snd_Nokia_Data(char *data, int invert);
+
+
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRB = 0xFF; PORTB = 0x00; 
+
+    /* Insert your solution below */
+
+    SPI_Init();
+    init_Screen();
+    clear_Screen();
+
+    //Initialize x&y
+    Snd_Nokia_Instructions(0x40);
+    Snd_Nokia_Instructions(0x80);
+
+    //Send Text, second Parameter is if to highlight the row
+    Snd_Nokia_Data("1. Start Game ",0);
+    Snd_Nokia_Data("1. Start Game ",1);
+    Snd_Nokia_Data("1. Start Game ",0);
+    Snd_Nokia_Data("1. Start Game ",1);
+    Snd_Nokia_Data("1. Start Game ",0);
+
+    while (1) {
+
+    }
+    return 1;
+}
+
+void SPI_Init(){
+    //From the data sheet
+    //Enable SPI,MASTR, set clock rate
+    SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
+}
+
+void SPI_Transmit(char cData){
+    //From the Data Sheet
+    //Start the transmission
+    //Shift each byte by writing it to the SPDR
+    SPDR = cData;
+
+    //Wait for transmission to complete
+    while(!(SPSR & (1<<SPIF)));
+}
+void Snd_Nokia_Instructions(char command){
+    //Set D/C to Zero to not write to screen and pass instructions.
+    PORTB &= ~(1<<DC);
+
+    //When SS is held low than the SPI is activated, and MISO becomes an ouput
+    PORTB &= ~(1<<SS);
+
+    //Send data command to the SPDR register and shift each bit
+    SPI_Transmit(command);
+
+    //When SS id driven high. all pins are inputs, and the SPI is passive, means
+    // that it will not recieve incoming data
+    PORTB |= (1<<SS); 
+}
+
+void Snd_Nokia_Data(char *data, int invert){
+    unsigned char temp = 0x00;
+    //enable DC to high to enable screen for data write to Y and X
+    PORTB |= (1<<DC);
+
+    //When SS is held low than the SPI is activated, and MISO becomes an ouput
+    PORTB &= ~(1<<SS);
+
+    //get the length of the char character to display each character in a 
+     // down up sequence starting with the first 6 column
+    int length = strlen(data);
+
+    for(int x=0; x<length; x++){//Cylce through each character
+        for(int y=0; y<6; y++){
+            temp = (invert) ? ~(ASCII[data[x]-32][y]) : (ASCII[data[x]-32][y]);
+            //Pass each column of data that consists of 6 columns that make up The character
+            //Subtract 32 since the library only has ASCII characters 32 - 125 and 
+            //the 2D array starts at 0 = 32 to offset it on the right character.
+            SPI_Transmit(temp);
+        }
+    }
+    //When SS is driven high, all pins are inputs, and the SPI is passive, means
+    //That it will not receive incoming dat
+    PORTB |= (1<<SS);
+}
+
+void init_Screen(){
+    //To activate Reset RST must be set low than back to high
+    PORTB &= ~(1<<RST);
+    _delay_ms(110); //Stated in the data sheet the there must be a delay
+                    //after RST is set to low than high
+    PORTB |= (1<<RST);
+
+    //Function set DB5=1 and H=1 extended instruction set to preset 
+    //the screen to specific parameters
+    Snd_Nokia_Instructions(0x21);
+
+    //Set Vop
+    Snd_Nokia_Instructions(0xC0);
+
+    //Set Voltage Bias to inverse video mode
+    Snd_Nokia_Instructions(0x13);
+
+    //Function Set PD = 0 and V = 0, Select Normal Mode instruction set
+    Snd_Nokia_Instructions(0x20);
+
+    //Display control set normal mode (D = 1 and E = 0)
+    Snd_Nokia_Instructions(0x0C);
+}
+
+void clear_Screen(){
+    //When SS is held low than the SPI is activated, and MISO becomes an ouput
+    PORTB &= ~(1<<SS);
+    PORTB |= (1<<DC);
+
+    for(int t=0; t< 504; t++){
+        //Clearing out each column since there are 
+        SPI_Transmit(0x00); 
+    }
+    PORTB &= ~(1<<DC);
+
+    //When SS is driven high, all pins are inputs, and the SPI is passive, means
+    //That it will not receive incoming dat
+    PORTB |= (1<<SS);
+}
+
+
