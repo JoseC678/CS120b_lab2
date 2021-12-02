fe2b7a2f495273dd81677867d99359e9b52afb0f
JoseC678 <cervantesjose657@gmail.com>
2021-12-02 02:13:21 -0800

    Finished Game added 3 Levels only so demo would be short

--- Project/source/main.c
+++ Project/source/main.c
@@ -21,6 +21,9 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #include "../header/font.h"
@@ -39,10 +42,17 @@ typedef struct _task{
 }task;
 
 unsigned char currPositionMenu = 0;
+unsigned char gameOn = 0;
 unsigned char outputMenu = 0;
 unsigned char menu_on_off = 1;
+unsigned char display_menuOnce =0;
+ int level = 1;
 void set_startingMenu();
 void set_PlayGame();
+void restArray();
+void win_Screen();
+void lose_Screen();
+void set_ShowScore(int lvl);
 
 
 
@@ -58,7 +68,7 @@ int JoyStickTick(int state){
     switch(state){
         case JoyStick_Init:state = JoyStick_Wait; break;
         case JoyStick_Wait:
-            if(menu_on_off==0){
+            if(menu_on_off==0 && gameOn == 1){
                 if(currPosition == output){
                     //If The current position is the same as the output than no movenment is detected
                     state = JoyStick_Wait;
@@ -69,13 +79,13 @@ int JoyStickTick(int state){
                     state = pause_Hold;
                 }
             }else{
-                PORTC = 0x00;
-                for(int l = 0; l < 8; l++){
-                    PORTC = 0x08;
-                    PORTC |= ((0b00001111>>l) & 0x01);
-                    PORTC |= 0x02;
-                } 
-                PORTC |= 0x04;
+               // PORTC = 0x00;
+               // for(int l = 0; l < 8; l++){
+              //      PORTC = 0x08;
+              //      PORTC |= ((0b00001111>>l) & 0x01);
+              //      PORTC |= 0x02;
+              //  } 
+              //  PORTC |= 0x04;
             }
             break;
         case Joystick_Update: break;
@@ -126,10 +136,6 @@ int JoyStickTick(int state){
     }
 
 
-
-    
-
-
     return state;
 
 
@@ -138,8 +144,13 @@ int JoyStickTick(int state){
 
 enum NokiaState{NokiaInit, Nokia_Wait, up_Hold, up_Release, down_Hold,down_Release, start_Hold, start_Release};
 int NokiaTick(int state){
+            if((~PIND & 0X08)==0X08){
+                set_startingMenu();
+            }
+
     switch(state){
         case NokiaInit: 
+            
             state =  Nokia_Wait;
             
             break;
@@ -209,9 +220,7 @@ int NokiaTick(int state){
             Snd_Nokia_Instructions(0x40);
             Snd_Nokia_Instructions(0x80);
             
-            //Send Text, second Parameter is if to highlight the row
-            Snd_Nokia_Data("1. LVL1       ",0);
-            Snd_Nokia_Data("2. Score: 1",0);
+            set_ShowScore(level);
             state = Nokia_Wait;
             break;
         default: break;
@@ -221,21 +230,163 @@ int NokiaTick(int state){
     
 }
 
+ int arraySelect[4][4]={{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
+ int arrayRecord[3][2]= {{0,0},{0,0},{0,0}};
+ int timeTick=0;
+ int tempLVL = 0;
+ int increment=0;
+ int xtemp=0;
+ int ytemp=0;
+enum GameState{gameInit,game_wait, generate_seq,display_seq, check_Responce,check_hold,check_release, nxt_Lvl, lose_game, win_Game};
 
+int GameTick(int state){
+    switch(state){
+        case gameInit:
+            restArray();
+            level = 1;
+            state = game_wait;
+            break;
+        case game_wait:
+            if(menu_on_off == 0){
+                state = generate_seq;
+            }else{
+                state = game_wait;
+            }
+            break;
+        case generate_seq:  break;
+        case display_seq: break;
+        case check_Responce:
+            if((~PIND & 0x04) == 4){
+                state = check_hold;
+            }else{
+                state = check_Responce;
+            }
+            
+            break;
+        case check_hold:break;
+        case check_release:
+            if(tempLVL >0){
+                state = check_Responce;
+                if(arrayRecord[tempLVL-1][0] == ii && arrayRecord[tempLVL-1][1] == jj){
+                    tempLVL--;
+                    state = check_Responce;
+        
+                }else{
+                    state = lose_game;
+                }
+                if(tempLVL ==0){
+                    level++;
+                    state = game_wait;
+                    restArray();
+                }
+                if(tempLVL==0 && level ==4){
+                    state = win_Game;
+                }
+                set_ShowScore(level);
+            }
+            break;
+        case win_Game: break;
+        case lose_game: break;
+        default: break;
+    }
+
+    switch(state){
+        case gameInit:break;
+        case game_wait:break;
+        case generate_seq:
+            for (int q=0; q<level; q++){
+                xtemp = rand() % 5;
+                ytemp = rand() % 5;
+                if(arraySelect[xtemp][ytemp]==1){
+                    q--;//Search again
+                }else{
+                    arrayRecord[q][0]=xtemp;
+                    arrayRecord[q][1]=ytemp;
+                    arraySelect[xtemp][ytemp] = 1;
+                }
+            }
+            state = display_seq;
+            tempLVL = level;
+            break;
+        case display_seq:
+            if(timeTick > 10){
+                timeTick=0;
+                increment++;
+                tempLVL--;
+            }else{
+                timeTick++;
+
+                PORTC = 0x00;
+                for(int l = 0; l < 8; l++){
+                    PORTC = 0x08;
+                    PORTC |= ((arrMatrix[arrayRecord[tempLVL-1][0]][arrayRecord[tempLVL-1][1]]>>l) & 0x01);
+                    PORTC |= 0x02;
+                } 
+                PORTC |= 0x04;
+
+            }
+
+            if(tempLVL == 0){
+                state = check_Responce;
+                gameOn = 1;
+
+                PORTC = 0x00;
+                for(int l = 0; l < 8; l++){
+                    PORTC = 0x08;
+                    PORTC |= ((0b10000111>>l) & 0x01);
+                    PORTC |= 0x02;
+                } 
+                PORTC |= 0x04;
+                tempLVL = level;
+            }
+            break;
+        case check_Responce:break;
+        case check_hold:
+            if((~PIND & 0x04) == 4){
+                state = check_hold;
+            }else{
+                state = check_release;
+            }
+            break;
+        case check_release:break;
+        case win_Game: 
+            win_Screen();
+            display_menuOnce =1;
+            if((~PIND & 0X08)==0X08){
+                set_startingMenu();
+            }
+            break;
+        case lose_game: 
+            lose_Screen();
+            display_menuOnce =1;
+            if((~PIND & 0X08)==0X08){
+                set_startingMenu();
+                state = gameInit;
+            }
+            break;
+        default:break;
+    }
+    if(menu_on_off==1){
+        state = gameInit;
+        //display_menuOnce =1;
+    }
+    return state;
+}
 
 int main(void){
+    srand(time(NULL));
     int i;
     DDRA = 0x00; PORTA = 0xFF; // A pins are used as inputs for the Joystick
     DDRC = 0x0F; PORTC = 0xF0; // C pins first 4 pins are used as outputs
     DDRB = 0xFF; PORTB = 0x00; 
     DDRD = 0x00; PORTD = 0xFF; // C pins first 4 pins are used as outputs
 
-    static task task1, task2;
-    task *tasks[] = {&task1, &task2}; 
+    static task task1, task2,task3;
+    task *tasks[] = {&task1, &task2,&task3}; 
     const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
 
     task1.state=JoyStick_Init;
-    task1.period=150;
+    task1.period=100;
     task1.elapsedTime = task1.period;
     task1.TickFct = &JoyStickTick;
 
@@ -243,6 +394,12 @@ int main(void){
     task2.period=50;
     task2.elapsedTime = task2.period;
     task2.TickFct = &NokiaTick;
+
+    task3.state=gameInit;
+    task3.period=50;
+    task3.elapsedTime = task3.period;
+    task3.TickFct = &GameTick;
+
     SPI_Init();
     init_Screen();
     set_startingMenu();
@@ -268,6 +425,30 @@ int main(void){
     return 1;
 }
 
+void lose_Screen(){
+    clear_Screen();
+
+    //Initialize x&y
+    Snd_Nokia_Instructions(0x40);
+    Snd_Nokia_Instructions(0x80);
+    
+    //Send Text, second Parameter is if to highlight the row
+    Snd_Nokia_Data("1. You LOSE!! ",1);
+    Snd_Nokia_Data("2. Press Red Button to play again  ",0);
+
+}
+
+void win_Screen(){
+    clear_Screen();
+
+    //Initialize x&y
+    Snd_Nokia_Instructions(0x40);
+    Snd_Nokia_Instructions(0x80);
+    
+    //Send Text, second Parameter is if to highlight the row
+    Snd_Nokia_Data("1. You WIN!!! ",1);
+    Snd_Nokia_Data("2. Score: 3        Press Red Button to play again  ",0);
+}
 
 void set_startingMenu(){
 
@@ -292,8 +473,63 @@ void set_PlayGame(){
     Snd_Nokia_Instructions(0x80);
     
     //Send Text, second Parameter is if to highlight the row
-    Snd_Nokia_Data("1. LVL1 ",0);
+    Snd_Nokia_Data("1. LVL1       ",0);
     Snd_Nokia_Data("2. Score: 1",0);
     
 
+}
+
+
+
+void set_ShowScore(int lvl){
+
+    clear_Screen();
+
+    //Initialize x&y
+    Snd_Nokia_Instructions(0x40);
+    Snd_Nokia_Instructions(0x80);
+    
+    //Send Text, second Parameter is if to highlight the row
+    if(lvl == 1){
+            Snd_Nokia_Data("1. LVL1       ",0);
+    Snd_Nokia_Data("2. Score: 0",0);
+    }
+    if(lvl == 2){
+            Snd_Nokia_Data("1. LVL2       ",0);
+    Snd_Nokia_Data("2. Score: 1",0);
+    }
+    if(lvl == 3){
+            Snd_Nokia_Data("1. LVL3       ",0);
+    Snd_Nokia_Data("2. Score: 2",0);
+    }
+
+    
+
+}
+
+void restArray(){
+
+ arraySelect[0][0]=0;
+ arraySelect[0][1]=0;
+ arraySelect[0][2]=0;
+ arraySelect[0][3]=0;
+ arraySelect[1][0]=0;
+ arraySelect[1][1]=0;
+ arraySelect[1][2]=0;
+ arraySelect[1][3]=0;
+ arraySelect[2][0]=0;
+ arraySelect[2][1]=0;
+ arraySelect[2][2]=0;
+ arraySelect[2][3]=0;
+ arraySelect[3][0]=0;
+ arraySelect[3][1]=0;
+ arraySelect[3][2]=0;
+ arraySelect[3][3]=0;
+
+ arrayRecord[0][0]= 0;
+ arrayRecord[0][1]= 0;
+ arrayRecord[1][0]= 0;
+ arrayRecord[1][1]= 0;
+ arrayRecord[2][0]= 0;
+ arrayRecord[2][1]= 0;
 }
